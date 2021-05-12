950fa527ca34cd1748581bc87b7cee26005b9e20
Alex George <alexgeorge2863@gmail.com>
2021-05-12 09:58:56 -0600

    initial start of reviews project. Import react-icons, reviews data and css

---
+++ react-projects/reviews/src/App.js
@@ -0,0 +1,8 @@
+import Review from './components/Review'
+export default function App() {
+	return (
+		<h2>
+			<Review />
+		</h2>
+	)
+}
---
+++ react-projects/reviews/src/components/Review.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import reviewsData from '../data/reviewsData'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+
+export default function Review() {
+	return <h2>review component</h2>
+}
+
+/*
+id: 1,
+		name: 'susan smith',
+		job: 'web developer',
+		image:
+			'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+		text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  */
