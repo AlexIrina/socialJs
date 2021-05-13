4bce6d9d1d0b970a9815124e5355dfc6ef185bd2
Alex George <alexgeorge2863@gmail.com>
2021-05-13 15:12:03 -0600

    add state, map over data, display data in the ui

--- react-projects/card-slider/src/App.js
+++ react-projects/card-slider/src/App.js
@@ -1,3 +1,46 @@
+import { useState, useEffect } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoteRight } from 'react-icons/fa'
+import peopleData from './data/peopleData'
+
 export default function App() {
-	return <div>App</div>
+	const [people, setPeople] = useState(peopleData)
+	const [index, setIndex] = useState(0)
+
+	return (
+		<section className='section'>
+			{/*  */}
+			<div className='title'>
+				<h2>
+					<span>/</span>reviews
+				</h2>
+			</div>
+			{/*  */}
+			<div className='section-center'>
+				{people.map(({ id, image, name, title, quote, personIndex }) => (
+					<article key={id}>
+						<img src={image} alt={name} className='person-img' />
+						<h4 className='title'>{name}</h4>
+						<p className='title'>{title}</p>
+						<p className='text'>{quote}</p>
+						<FaQuoteRight className='icon' />
+					</article>
+				))}
+				{/*  */}
+				<button className='prev'>
+					<FiChevronLeft />
+				</button>
+				<button className='next'>
+					<FiChevronRight />
+				</button>
+			</div>
+		</section>
+	)
 }
+/*
+ 	id: 
+  image:
+  name: 
+  title: 
+  quote:
+  */
