329a77806d736b5730e279eade9c642ebe985a5e
Alex George <alexgeorge2863@gmail.com>
2021-05-18 13:29:48 -0600

    set up basic UI with form

--- react-projects/todo-list/src/App.js
+++ react-projects/todo-list/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import List from './components/List'
+import Alert from './components/Alert'
 
 export default function App() {
 	//form values
@@ -13,11 +14,27 @@ export default function App() {
 	// alert..for different alerts
 	const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
 
+	// handle submit for the form
+	const handleSubmit = e => {
+		e.preventDefault()
+		console.log('form submitted')
+	}
+
 	return (
-		<div>
-			App
-			<List />
-		</div>
+		<section className='section-center'>
+			<form className='grocery-form ' onSubmit={handleSubmit}>
+				{/* show different alerts to the user */}
+				{}
+				<input className='grocery' type='text' placeholder='e.g. eggs' />
+				<button className='submit-btn'>Submit</button>
+			</form>
+			<div className='grocery-container'>
+				<List />
+				<button className='clear-btn' onClick={() => setList([])}>
+					Clear Items
+				</button>
+			</div>
+		</section>
 	)
 }
 // using local storage
--- react-projects/todo-list/src/components/List.js
+++ react-projects/todo-list/src/components/List.js
@@ -1,6 +1,7 @@
 import { FaEdit, FaTrash } from 'react-icons/fa'
 
 export default function List() {
+	//! main ?
 	return <div>List component</div>
 }
 // todo: iterate over data here
