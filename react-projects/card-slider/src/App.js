909bb6da795ad28882c909d0354ffdd563307f6e
Alex George <alexgeorge2863@gmail.com>
2021-05-18 12:47:18 -0600

    start of todo project. add css, List and Error components

--- react-projects/card-slider/src/App.js
+++ react-projects/card-slider/src/App.js
@@ -1,46 +1,65 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
 import { FaQuoteRight } from 'react-icons/fa'
 import peopleData from './data/peopleData'
-
-export default function App() {
+function App() {
 	const [people, setPeople] = useState(peopleData)
-	const [index, setIndex] = useState(0)
+	const [index, setIndex] = React.useState(0)
+
+	useEffect(() => {
+		const lastIndex = people.length - 1
+		if (index < 0) setIndex(lastIndex)
+		if (index > lastIndex) setIndex(0)
+	}, [index, people])
+
+	useEffect(() => {
+		let slider = setInterval(() => {
+			setIndex(index + 1)
+		}, 5000)
+		return () => {
+			clearInterval(slider)
+		}
+	}, [index])
 
 	return (
 		<section className='section'>
-			{/*  */}
 			<div className='title'>
 				<h2>
 					<span>/</span>reviews
 				</h2>
 			</div>
-			{/*  */}
 			<div className='section-center'>
-				{people.map(({ id, image, name, title, quote, personIndex }) => (
-					<article key={id}>
-						<img src={image} alt={name} className='person-img' />
-						<h4 className='title'>{name}</h4>
-						<p className='title'>{title}</p>
-						<p className='text'>{quote}</p>
-						<FaQuoteRight className='icon' />
-					</article>
-				))}
-				{/*  */}
-				<button className='prev'>
+				{people.map(({ id, image, name, title, quote }, personIndex) => {
+					// changing based on the css class
+					let position = 'nextSlide'
+					if (personIndex === index) position = 'activeSlide'
+
+					if (
+						personIndex === index - 1 ||
+						(index === 0 && personIndex === people.length - 1)
+					) {
+						position = 'lastSlide'
+					}
+
+					return (
+						<article className={position} key={id}>
+							<img src={image} alt={name} className='person-img' />
+							<h4>{name}</h4>
+							<p className='title'>{title}</p>
+							<p className='text'>{quote}</p>
+							<FaQuoteRight className='icon' />
+						</article>
+					)
+				})}
+				<button className='prev' onClick={() => setIndex(index - 1)}>
 					<FiChevronLeft />
 				</button>
-				<button className='next'>
+				<button className='next' onClick={() => setIndex(index + 1)}>
 					<FiChevronRight />
 				</button>
 			</div>
 		</section>
 	)
 }
-/*
- 	id: 
-  image:
-  name: 
-  title: 
-  quote:
-  */
+
+export default App
---
+++ react-projects/todo-list/src/App.js
@@ -0,0 +1,10 @@
+import List from './components/List'
+
+export default function App() {
+	return (
+		<div>
+			App
+			<List />
+		</div>
+	)
+}
