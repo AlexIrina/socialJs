5edb3bcac2f29e54e68e553ec915b1dd73564e07
Alex George <alexgeorge2863@gmail.com>
2021-05-21 08:15:27 -0600

    map over social icons and links data insteadof hardcoding it

--- react-projects/navbar/src/components/Navbar.js
+++ react-projects/navbar/src/components/Navbar.js
@@ -3,48 +3,47 @@ import { FaBars, FaTwitter } from 'react-icons/fa'
 import { links, social } from '../data/linksData'
 import logo from '../logo.svg'
 export default function Navbar() {
+	// toggles links container
+	const [showLinks, setShowLinks] = useState(false)
+
+	// slows down the way links container toggles open
+	useEffect(() => {
+		setTimeout(() => {
+			setShowLinks()
+		}, 3000)
+	}, [])
+
 	return (
 		<nav>
 			<div className='nav-center'>
 				<div className='nav-header'>
 					<img src={logo} alt='logo' />
-					<button className='nav-toggle'>
+					<button
+						className='nav-toggle'
+						// ! gets the opposite value for toggle to work
+						onClick={() => setShowLinks(!showLinks)}
+					>
 						<FaBars />
 					</button>
 				</div>
-				{/*//!todo: show container will show the links container*/}
+				{/* {showLinks && ( */}
 				<div className='links-container show-container'>
 					<ul className='links'>
-						<li>
-							<a href='#'>home</a>
-						</li>
-						<li>
-							<a href='#'>about</a>
-						</li>
-						<li>
-							<a href='#'>service</a>
-						</li>
-						<li>
-							<a href='#'>products</a>
-						</li>
+						{links.map(({ id, url, text }) => (
+							<li key={id}>
+								<a href={url}>{text}</a>
+							</li>
+						))}
 					</ul>
 				</div>
+				{/* )} */}
+
 				<ul className='social-icons'>
-					<li>
-						<a href='https://www.twitter.com'>
-							<FaTwitter />
-						</a>
-					</li>
-					<li>
-						<a href='https://www.twitter.com'>
-							<FaTwitter />
-						</a>
-					</li>
-					<li>
-						<a href='https://www.twitter.com'>
-							<FaTwitter />
-						</a>
-					</li>
+					{social.map(({ id, url, icon }) => (
+						<li key={id}>
+							<a href={url}>{icon}</a>
+						</li>
+					))}
 				</ul>
 			</div>
 		</nav>
