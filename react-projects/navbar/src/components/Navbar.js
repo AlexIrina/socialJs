2a9b851e82182d5ce0cd989a455dbdb3b0983e5e
Alex George <alexgeorge2863@gmail.com>
2021-05-21 07:41:01 -0600

    Basic ui setup. styling nav, import social media links

---
+++ react-projects/navbar/src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { FaBars, FaTwitter } from 'react-icons/fa'
+import { links, social } from '../data/linksData'
+import logo from '../logo.svg'
+export default function Navbar() {
+	return (
+		<nav>
+			<div className='nav-center'>
+				<div className='nav-header'>
+					<img src={logo} alt='logo' />
+					<button className='nav-toggle'>
+						<FaBars />
+					</button>
+				</div>
+				{/*//!todo: show container will show the links container*/}
+				<div className='links-container show-container'>
+					<ul className='links'>
+						<li>
+							<a href='#'>home</a>
+						</li>
+						<li>
+							<a href='#'>about</a>
+						</li>
+						<li>
+							<a href='#'>service</a>
+						</li>
+						<li>
+							<a href='#'>products</a>
+						</li>
+					</ul>
+				</div>
+				<ul className='social-icons'>
+					<li>
+						<a href='https://www.twitter.com'>
+							<FaTwitter />
+						</a>
+					</li>
+					<li>
+						<a href='https://www.twitter.com'>
+							<FaTwitter />
+						</a>
+					</li>
+					<li>
+						<a href='https://www.twitter.com'>
+							<FaTwitter />
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	)
+}
