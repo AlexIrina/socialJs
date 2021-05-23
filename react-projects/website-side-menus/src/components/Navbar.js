1ba4ba3fc8f2c8d288cfc42a81e59523ca12275f
Alex George <alexgeorge2863@gmail.com>
2021-05-23 08:55:09 -0600

    add navbar.Import openSidebar, closeSidebar, openSubmenu, closeSubmenu context into navbar

--- react-projects/website-side-menus/src/components/Navbar.js
+++ react-projects/website-side-menus/src/components/Navbar.js
@@ -1,6 +1,33 @@
 import logo from '../images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from '../context/appContext'
 
 export default function Navbar() {
-	return <h2>navbar component</h2>
+	const { openSidebar, closeSidebar, openSubmenu, closeSubmenu } =
+		useGlobalContext()
+
+	return (
+		<nav className='nav'>
+			<div className='nav-center'>
+				<div className='nav-header'>
+					<img src={logo} alt='stripe' className='nav-logo' />
+					<button className='btn toggle-btn' onClick={openSidebar}>
+						<FaBars />
+					</button>
+				</div>
+				<ul className='nav-links'>
+					<li>
+						<button className='link-btn'>products</button>
+					</li>
+					<li>
+						<button className='link-btn'>developers</button>
+					</li>
+					<li>
+						<button className='link-btn'>company</button>
+					</li>
+				</ul>
+				<button className='btn signIn-btn'>Sign in</button>
+			</div>
+		</nav>
+	)
 }
