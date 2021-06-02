7fcaf0f493203d4ef3a1b29c01d24385f2d0d32b
Alex George <alexgeorge2863@gmail.com>
2021-06-02 11:46:42 -0600

    add search input that allows the user to search for any cocktail in the api

--- react-projects/cocktails--all/src/components/SearchForm.js
+++ react-projects/cocktails--all/src/components/SearchForm.js
@@ -1,6 +1,35 @@
+import { useRef, useEffect } from 'react'
 import { useGlobalContext } from '../context'
 
 export default function SearchForm() {
 	const { setSearchTerm } = useGlobalContext()
-	return <form onSubmit={setSearchTerm}></form>
+	const searchValues = useRef('')
+
+	// !every time the user types something in the search. setSearchTerm will fire
+	const searchCocktail = () => {
+		setSearchTerm(searchValues.current.value)
+	}
+	const handleSubmit = e => {
+		e.preventDefault()
+	}
+
+	//! set focus on the input when the app renders
+	useEffect(() => {
+		searchValues.current.focus()
+	}, [])
+	return (
+		<section className='section search'>
+			<form className='search-form' onSubmit={handleSubmit}>
+				<div className='form-control'>
+					<label htmlFor='name'>Search your favorite cocktail</label>
+					<input
+						type='text'
+						id='name'
+						ref={searchValues}
+						onChange={searchCocktail}
+					/>
+				</div>
+			</form>
+		</section>
+	)
 }
