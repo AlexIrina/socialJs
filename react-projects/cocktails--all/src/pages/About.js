3e182af30a3a9b6cb326723b5ebb667d1bbd2ce2
Alex George <alexgeorge2863@gmail.com>
2021-06-02 08:51:34 -0600

    add Error and About pages with styling and links

--- react-projects/cocktails--all/src/pages/About.js
+++ react-projects/cocktails--all/src/pages/About.js
@@ -1,7 +1,14 @@
 export default function About() {
 	return (
-		<div>
-			<h2>about page</h2>
-		</div>
+		<section className='section about-section'>
+			<h1 className='section-title'>About Us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. A ratione iure
+				fugit voluptas architecto est esse neque delectus vero non, beatae
+				laboriosam laudantium libero cupiditate nulla explicabo, cum nemo minima
+				quae! Corporis necessitatibus nihil commodi expedita voluptatum dolorem
+				iste obcaecati.
+			</p>
+		</section>
 	)
 }
--- react-projects/cocktails--all/src/pages/Error.js
+++ react-projects/cocktails--all/src/pages/Error.js
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom'
 
 export default function Error() {
 	return (
-		<div>
-			<h2>error page</h2>
-			<Link to='/' className='btn'>
-				Back to home
-			</Link>
-		</div>
+		<section className='error-page section'>
+			<div className='error-container'>
+				<h1>Opps! It's a dead end</h1>
+				<Link to='/' className='btn btn-primary'>
+					Back home
+				</Link>
+			</div>
+		</section>
 	)
 }
